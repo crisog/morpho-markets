7a6cc2ba216402231987641fc88616b04d00b1af
crisog <ortega.cpp@gmail.com>
2024-11-26 15:38:37 -0400

    fix: id must be from args not logs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as schema from "../ponder.schema";
 
 ponder.on("Morpho:CreateMarket", async ({ event, context }) => {
   await context.db.insert(schema.createMarketEvent).values({
-    id: event.log.id,
+    id: event.args.id,
     loanToken: event.args.marketParams.loanToken,
     collateralToken: event.args.marketParams.collateralToken,
     oracle: event.args.marketParams.oracle,
