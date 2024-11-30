6f8b1ed7f6f488d2f3daa8b86dd36bddafb17677
crisog <ortega.cpp@gmail.com>
2024-11-30 15:33:38 -0400

    feat: adds base to config

--- ponder.config.ts
+++ ponder.config.ts
@@ -15,12 +15,27 @@ export default createConfig({
         }
       ),
     },
+    base: {
+      chainId: 8453,
+      transport: rateLimit(
+        http(process.env.BASE_RPC_URL, { retryCount: 1, timeout: 1_000 }),
+        {
+          requestsPerSecond: 100,
+        }
+      ),
+    },
   },
   contracts: {
     Morpho: {
       network: "mainnet",
       abi: MorphoAbi,
       startBlock: 18883124,
+      address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
+    },
+    MorphoBase: {
+      network: "base",
+      abi: MorphoAbi,
+      startBlock: 13977148,
       address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
     },
   },
